8ba2c2b8e1a6d8b2bdce843e6adc4b2d679fbc10
Nemhesis-Idrissa <idrissainjai11@gmail.com>
2026-01-08 16:12:01 +0100

    Stop tracking build files

--- build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/deploy_AegisSecurity_2aeaa7b942.cmake
+++
@@ -1,8 +0,0 @@
-include(C:/Users/idris/Desktop/Documents/Aegis/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake)
-include("${CMAKE_CURRENT_LIST_DIR}/AegisSecurity-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase;qtmultimedia;qtmultimedia")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE C:/Users/idris/Desktop/Documents/Aegis/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/AegisSecurity.exe
-    GENERATE_QT_CONF
-)
